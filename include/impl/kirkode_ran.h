1135b72b8e4f474482c320fcd29f1de9e4fa32e9
Nathan Kirby <166115337+NathanKirby@users.noreply.github.com>
2026-06-23 20:40:49 -0700

    Added documentation, nodiscard messages

--- include/impl/kirkode_ran.h
+++ include/impl/kirkode_ran.h
@@ -11,20 +11,31 @@
 #endif
 
 namespace kir {
+	/**
+	 * \class ran
+	 * \brief Static random number generation utility based on Mersenne Twister.
+	 *
+	 * Provides thread-shared pseudo-random generation for integers, floating-point
+	 * values, and raw byte sequences.
+	 *
+	 * The generator is seeded once using std::random_device and reused globally.
+	 */
 	class ran {
 	private:
 		inline static std::random_device rd;
 		inline static std::mt19937 engine = std::mt19937(rd());
 	public:
 		/**
-		* Generates a random integral value spanning the full range of the specified integral type.
+		* \brief Generates a random integral value spanning the full range of the specified integral type.
 		* 
 		* \tparam IntType: Integral type used to define the range of possible values.
+		* uint32_t by default.
 		* 
 		* \return A randomly generated value of type IntType within the inclusive range
 		* [std::numeric_limits<IntType>::min(), std::numeric_limits<IntType>::max()].
 		*/
 		template<typename IntType = uint32_t>
+		[[nodiscard("kir::ran::random_int() is useless without use of its return value.")]]
 		static IntType random_int() noexcept {
 			static_assert(std::is_integral<IntType>::value, "random_int only supports integral types!");
 			if constexpr (std::is_signed<IntType>::value) {
@@ -44,17 +55,19 @@ namespace kir {
 		}
 
 		/**
-		* Generates a random integral value within a range starting from the minimum representable value
+		* \brief Generates a random integral value within a range starting from the minimum representable value
 		* of the specified type up to a user-defined maximum bound.
 		*
 		* \tparam IntType: Integral type used to define the range of possible values.
-		*
+		* uint32_t by default.
+		* 
 		* \param max: Upper inclusive bound of the random range.
 		*
 		* \return A randomly generated value of type IntType within the inclusive range
 		* [std::numeric_limits<IntType>::min(), max].
 		*/
 		template<typename IntType = uint32_t>
+		[[nodiscard("kir::ran::random_int() is useless without use of its return value.")]]
 		static IntType random_int(const IntType max) noexcept {
 			static_assert(std::is_integral<IntType>::value, "random_int only supports integral types!");
 			if constexpr (std::is_signed<IntType>::value) {
@@ -74,7 +87,7 @@ namespace kir {
 		}
 
 		/**
-		* Generates a random integral value within a user-specified inclusive range.
+		* \brief Generates a random integral value within a user-specified inclusive range.
 		*
 		* \tparam IntType: Integral type used to define the range of possible values.
 		*
@@ -84,6 +97,7 @@ namespace kir {
 		* \return A randomly generated value of type IntType within the inclusive range [min, max].
 		*/
 		template<typename IntType = uint32_t>
+		[[nodiscard("kir::ran::random_int() is useless without use of its return value.")]]
 		static IntType random_int(const IntType min, const IntType max) noexcept {
 			static_assert(std::is_integral<IntType>::value, "random_int only supports integral types!");
 			if constexpr (std::is_signed<IntType>::value) {
@@ -103,14 +117,16 @@ namespace kir {
 		}
 
 		/**
-		* Generates a random floating point spanning the full range of the specified float type.
+		* \brief Generates a random floating point spanning the full range of the specified float type.
 		*
 		* \tparam FloatType: Float type used to define the range of possible values.
-		*
+		* float by default.
+		* 
 		* \return A randomly generated value of type FloatType within the inclusive range
 		* [std::numeric_limits<FloatType>::min(), std::numeric_limits<FloatType>::max()].
 		*/
 		template<typename FloatType = float>
+		[[nodiscard("kir::ran::random_float() is useless without use of its return value.")]]
 		static FloatType random_float() noexcept {
 			static_assert(std::is_floating_point<FloatType>::value, "random_float only supports floating point types!");
 			std::uniform_real_distribution<double> dist(
@@ -121,10 +137,11 @@ namespace kir {
 		}
 
 		/**
-		* Generates a random floating point within a range starting from the minimum representable value
+		* \brief Generates a random floating point within a range starting from the minimum representable value
 		* of the specified type up to a user-defined maximum bound.
 		*
 		* \tparam FloatType: Float type used to define the range of possible values.
+		* float by default.
 		*
 		* \param max: Upper inclusive bound of the random range.
 		*
@@ -132,6 +149,7 @@ namespace kir {
 		* [std::numeric_limits<FloatType>::min(), max].
 		*/
 		template<typename FloatType = float>
+		[[nodiscard("kir::ran::random_float() is useless without use of its return value.")]]
 		static FloatType random_float(const FloatType max) noexcept {
 			static_assert(std::is_floating_point<FloatType>::value, "random_float only supports floating point types!");
 			std::uniform_real_distribution<double> dist(
@@ -142,9 +160,10 @@ namespace kir {
 		}
 
 		/**
-		* Generates a random floating point within a user-specified inclusive range.
+		* \brief Generates a random floating point within a user-specified inclusive range.
 		*
 		* \tparam FloatType: Float type used to define the range of possible values.
+		* float by default.
 		*
 		* \param min: Lower inclusive bound of the random range.
 		* \param max: Upper inclusive bound of the random range.
@@ -152,6 +171,7 @@ namespace kir {
 		* \return A randomly generated value of type FloatType within the inclusive range [min, max].
 		*/
 		template<typename FloatType = float>
+		[[nodiscard("kir::ran::random_float() is useless without use of its return value.")]]
 		static FloatType random_float(const FloatType min, const FloatType max) noexcept {
 			static_assert(std::is_floating_point<FloatType>::value, "random_float only supports floating point types!");
 			std::uniform_real_distribution<double> dist(
@@ -162,12 +182,13 @@ namespace kir {
 		}
 
 		/**
-		* Generates a hash of random bytes.
+		* \brief Generates a hash of random bytes.
 		* 
 		* \param len: Length of random hash.
 		* 
 		* \return A randomly generated hash.
 		*/
+		[[nodiscard("kir::ran::random_bytes() is useless without use of its return value.")]]
 		static kir::bytes random_bytes(const size_t len) noexcept;
 	};
 }
